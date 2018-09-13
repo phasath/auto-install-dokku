10f88bf83c094b48b66b589ea93e238b8d9235c0
phasath <sathler93@gmail.com>
2018-09-13 11:42:28 -0300

    Configurando o ssh

--- install.sh
+++ install.sh
@@ -19,4 +19,21 @@ else
 			yum update ;
 			yum upgrade ;
 		fi
+
+		#Configuring the SSH
+		echo -e "\n\tSSH" ;
+		echo -e "\t\tCopying files..." ;
+		cp -r ssh/* /etc/ssh ;
+
+		echo -e "\t\tSetting permissions..." ;
+		chmod 600 /etc/ssh/sshd_config ;
+		chmod 640 /etc/ssh/ssh_host_* ;
+		# TIC gave us machines that holds a group "ssh_keys" that are owners of private ssh files
+		if [ $(getent group ssh_keys) ]; then
+			chown root:ssh_keys /etc/ssh/ssh_host_* ;
+		else
+			chown root:root /etc/ssh/ssh_host_* ;
+		fi
+		chown root:root /etc/ssh/ssh_host_*.pub ;
+		chmod 644 /etc/ssh/ssh_host_*.pub ;
 fi
