f3294ad425f4733599c083cc1d752d4a4c7d1250
phasath <sathler93@gmail.com>
2018-09-13 11:41:32 -0300

    Atualiza os pacotes do sistema

--- install.sh
+++ install.sh
@@ -10,4 +10,13 @@ else
 		# Configuring the server with necessary files
 		echo -e "Configuring the server"
 
+		#Running updates
+		echo -e "Updating the packages"
+		if [ $(python -mplatform | grep -i Ubuntu) ]; then
+			apt-get update ;
+			apt-get upgrade ;
+		else
+			yum update ;
+			yum upgrade ;
+		fi
 fi
